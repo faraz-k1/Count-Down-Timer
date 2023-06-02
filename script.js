46af9ea79225c23e1b0083f3c1de73284fef85f2
Faraz Khalid <120940968+faraz-k1@users.noreply.github.com>
2023-06-02 18:31:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const endDate = "1 June 2023 6:02 PM";
+const startTime = "1 June 2023 5:00 PM";
+
+document.getElementById("endDate").innerHTML = endDate;
+const inputs = document.querySelectorAll("input");
+
+function clock() {
+  const end = new Date(endDate);
+  const start = new Date();
+  const diff = end - start;
+  if (diff < 0) return;
+  inputs[0].value = Math.floor(diff / (1000 * 3600 * 24));
+  inputs[1].value = Math.floor((diff % (1000 * 3600 * 24)) / (1000 * 3600));
+  inputs[2].value = Math.floor((diff % (1000 * 3600)) / (1000 * 60));
+  inputs[3].value = Math.floor((diff % (1000 * 60)) / 1000);
+}
+
+clock();
+
+setInterval(() => {
+  clock();
+}, 1000);
